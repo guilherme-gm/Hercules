1e9f34f3d73a3f4d8d93ede651566ba51f4b2212
Guilherme G. Menaldo <guilherme.menaldo@outlook.com>
2023-08-23 23:22:09 -0300

    renewal autospell tests

--- src/plugins/test_autospell.c
+++ src/plugins/test_autospell.c
@@ -445,7 +445,7 @@ static bool test_autospell_cast_pc(void)
 
 #endif
 
-
+#ifndef RENEWAL
 static bool test_autospell_pc_selected(void)
 {
 	bool passed = true;
@@ -666,6 +666,253 @@ static bool test_autospell_pc_selected(void)
 
 	return passed;
 }
+
+#else
+
+static bool test_autospell_pc_selected(void)
+{
+	bool passed = true;
+
+	struct pc_skill all_skills[] = {
+		{ MG_COLDBOLT, 10 },
+		{ MG_FIREBOLT, 10 },
+		{ MG_LIGHTNINGBOLT, 10 },
+		{ MG_SOULSTRIKE, 10 },
+		{ MG_FIREBALL, 10 },
+		{ WZ_EARTHSPIKE, 10 },
+		{ MG_FROSTDIVER, 10 },
+		{ MG_THUNDERSTORM, 10 },
+		{ WZ_HEAVENDRIVE, 10 },
+	};
+
+	struct pc_skill autospell_sk = { SA_AUTOSPELL, 1 };
+
+	{
+		context("Player doesn't have AutoSpell but selected a skill (exploit?)");
+
+		use_fake_sc_start();
+		
+		struct map_session_data *sd = make_dummy_pc();
+		force_pc_addskill_list(sd, all_skills, ARRAYLENGTH(all_skills));
+		skill->autospell_spell_selected(sd, MG_FIREBOLT);
+
+		expect("it should not start a SC", fake_sc_start_val.type, 0);
+
+		reset_mocks();
+		clear_pc(sd);
+	}
+
+	{
+		context("Player tries to AutoSpell a skill they don't have (exploit?)");
+
+		use_fake_sc_start();
+		
+		struct map_session_data *sd = make_dummy_pc();
+		force_pc_addskill(sd, &autospell_sk);
+
+		skill->autospell_spell_selected(sd, MG_FIREBOLT);
+
+		expect("it should not start a SC", fake_sc_start_val.type, 0);
+
+		reset_mocks();
+		clear_pc(sd);
+	}
+
+	struct {
+		int autospell_level;
+		int skill_id;
+		int expected_level;
+	} skill_tests[] = {
+		{ 1, MG_COLDBOLT,      1 },
+		{ 1, MG_FIREBOLT,      1 },
+		{ 1, MG_LIGHTNINGBOLT, 1 },
+		{ 1, MG_SOULSTRIKE,    0 },
+		{ 1, MG_FIREBALL,      0 },
+		{ 1, WZ_EARTHSPIKE,    0 },
+		{ 1, MG_FROSTDIVER,    0 },
+		{ 1, MG_THUNDERSTORM,  0 },
+		{ 1, WZ_HEAVENDRIVE,   0 },
+
+		{ 2, MG_COLDBOLT,      1 },
+		{ 2, MG_FIREBOLT,      1 },
+		{ 2, MG_LIGHTNINGBOLT, 1 },
+		{ 2, MG_SOULSTRIKE,    0 },
+		{ 2, MG_FIREBALL,      0 },
+		{ 2, WZ_EARTHSPIKE,    0 },
+		{ 2, MG_FROSTDIVER,    0 },
+		{ 2, MG_THUNDERSTORM,  0 },
+		{ 2, WZ_HEAVENDRIVE,   0 },
+
+		{ 3, MG_COLDBOLT,      1 },
+		{ 3, MG_FIREBOLT,      1 },
+		{ 3, MG_LIGHTNINGBOLT, 1 },
+		{ 3, MG_SOULSTRIKE,    0 },
+		{ 3, MG_FIREBALL,      0 },
+		{ 3, WZ_EARTHSPIKE,    0 },
+		{ 3, MG_FROSTDIVER,    0 },
+		{ 3, MG_THUNDERSTORM,  0 },
+		{ 3, WZ_HEAVENDRIVE,   0 },
+
+		{ 4, MG_COLDBOLT,      2 },
+		{ 4, MG_FIREBOLT,      2 },
+		{ 4, MG_LIGHTNINGBOLT, 2 },
+		{ 4, MG_SOULSTRIKE,    2 },
+		{ 4, MG_FIREBALL,      2 },
+		{ 4, WZ_EARTHSPIKE,    0 },
+		{ 4, MG_FROSTDIVER,    0 },
+		{ 4, MG_THUNDERSTORM,  0 },
+		{ 4, WZ_HEAVENDRIVE,   0 },
+
+		{ 5, MG_COLDBOLT,      2 },
+		{ 5, MG_FIREBOLT,      2 },
+		{ 5, MG_LIGHTNINGBOLT, 2 },
+		{ 5, MG_SOULSTRIKE,    2 },
+		{ 5, MG_FIREBALL,      2 },
+		{ 5, WZ_EARTHSPIKE,    0 },
+		{ 5, MG_FROSTDIVER,    0 },
+		{ 5, MG_THUNDERSTORM,  0 },
+		{ 5, WZ_HEAVENDRIVE,   0 },
+
+		{ 6, MG_COLDBOLT,      3 },
+		{ 6, MG_FIREBOLT,      3 },
+		{ 6, MG_LIGHTNINGBOLT, 3 },
+		{ 6, MG_SOULSTRIKE,    3 },
+		{ 6, MG_FIREBALL,      3 },
+		{ 6, WZ_EARTHSPIKE,    0 },
+		{ 6, MG_FROSTDIVER,    0 },
+		{ 6, MG_THUNDERSTORM,  0 },
+		{ 6, WZ_HEAVENDRIVE,   0 },
+
+		{ 7, MG_COLDBOLT,      3 },
+		{ 7, MG_FIREBOLT,      3 },
+		{ 7, MG_LIGHTNINGBOLT, 3 },
+		{ 7, MG_SOULSTRIKE,    3 },
+		{ 7, MG_FIREBALL,      3 },
+		{ 7, WZ_EARTHSPIKE,    3 },
+		{ 7, MG_FROSTDIVER,    3 },
+		{ 7, MG_THUNDERSTORM,  0 },
+		{ 7, WZ_HEAVENDRIVE,   0 },
+
+		{ 8, MG_COLDBOLT,      4 },
+		{ 8, MG_FIREBOLT,      4 },
+		{ 8, MG_LIGHTNINGBOLT, 4 },
+		{ 8, MG_SOULSTRIKE,    4 },
+		{ 8, MG_FIREBALL,      4 },
+		{ 8, WZ_EARTHSPIKE,    4 },
+		{ 8, MG_FROSTDIVER,    4 },
+		{ 8, MG_THUNDERSTORM,  0 },
+		{ 8, WZ_HEAVENDRIVE,   0 },
+
+		{ 9, MG_COLDBOLT,      4 },
+		{ 9, MG_FIREBOLT,      4 },
+		{ 9, MG_LIGHTNINGBOLT, 4 },
+		{ 9, MG_SOULSTRIKE,    4 },
+		{ 9, MG_FIREBALL,      4 },
+		{ 9, WZ_EARTHSPIKE,    4 },
+		{ 9, MG_FROSTDIVER,    4 },
+		{ 9, MG_THUNDERSTORM,  0 },
+		{ 9, WZ_HEAVENDRIVE,   0 },
+
+		{ 10, MG_COLDBOLT,      5 },
+		{ 10, MG_FIREBOLT,      5 },
+		{ 10, MG_LIGHTNINGBOLT, 5 },
+		{ 10, MG_SOULSTRIKE,    5 },
+		{ 10, MG_FIREBALL,      5 },
+		{ 10, WZ_EARTHSPIKE,    5 },
+		{ 10, MG_FROSTDIVER,    5 },
+		{ 10, MG_THUNDERSTORM,  5 },
+		{ 10, WZ_HEAVENDRIVE,   5 },
+	};
+
+	for (int i = 0; i < ARRAYLENGTH(skill_tests); i += 9)
+	{
+		context("Player uses AutoSpell Lv%d and knows all skills at max level", skill_tests[i].autospell_level);
+		autospell_sk.skill_lv = skill_tests[i].autospell_level;
+
+		for (int j = i; j < i + 9; j++) {
+			use_fake_sc_start();
+			
+			struct map_session_data *sd = make_dummy_pc();
+			sd->menuskill_val =  skill_tests[i].autospell_level;
+			force_pc_addskill(sd, &autospell_sk);
+			for (int k = 0; k < ARRAYLENGTH(all_skills); k++)
+				force_pc_addskill(sd, &all_skills[k]);
+			// force_pc_addskill_list(sd, all_skills, ARRAYLENGTH(all_skills));
+
+			skill->autospell_spell_selected(sd, skill_tests[j].skill_id);
+
+			if (skill_tests[j].expected_level > 0) {
+				expect("it should start %s", fake_sc_start_val.val2, skill_tests[j].skill_id,
+					skill->get_name(skill_tests[j].skill_id));
+				expect("it should start %s on level %d", fake_sc_start_val.val3, skill_tests[j].expected_level,
+					skill->get_name(skill_tests[j].skill_id), skill_tests[j].expected_level);
+			} else {
+				// Note: This fails in current Herc code due to a bug, when picking a skill that you don't
+				// have enough autospell level to use, it gives Lv1.
+				expect("it should not start %s", fake_sc_start_val.val2, 0,
+					skill->get_name(skill_tests[j].skill_id));
+			}
+
+			reset_mocks();
+			clear_pc(sd);
+		}
+	}
+
+	{
+		context("AutoSpell Lv10 is used, and Soul Strike is selected when player only knows it on Lv1");
+
+		use_fake_sc_start();
+		
+		struct map_session_data *sd = make_dummy_pc();
+		sd->menuskill_val = 10;
+		autospell_sk.skill_lv = 10;
+		force_pc_addskill(sd, &autospell_sk);
+
+		struct pc_skill soul_strike = { MG_SOULSTRIKE, 1 };
+		force_pc_addskill(sd, &soul_strike);
+
+		skill->autospell_spell_selected(sd, MG_SOULSTRIKE);
+
+		expect("it should start %s", fake_sc_start_val.val2, MG_SOULSTRIKE,
+			skill->get_name(MG_SOULSTRIKE));
+		expect("it should start %s on level 1", fake_sc_start_val.val3, 1,
+			skill->get_name(MG_SOULSTRIKE));
+
+		reset_mocks();
+		clear_pc(sd);
+	}
+
+	// { Causes issues in run time -- test this by hand
+	// 	context("AutoSpell Lv10 is used, Player has Soul Link and chooses Cold Bolt (which he has it on Lv8)");
+
+	// 	struct map_session_data *sd = make_dummy_pc();
+	// 	set_pos(&sd->bl, 150, 150);
+	// 	sd->menuskill_val = 10;
+	// 	autospell_sk.skill_lv = 10;
+	// 	force_pc_addskill(sd, &autospell_sk);
+
+	// 	struct pc_skill coldbolt = { MG_COLDBOLT, 8 };
+	// 	force_pc_addskill(sd, &coldbolt);
+
+	// 	sc_start2(&sd->bl, &sd->bl, SC_SOULLINK, 100, 1, SL_SAGE, 10000, SL_SAGE);
+
+	// 	use_fake_sc_start();
+
+	// 	skill->autospell_spell_selected(sd, MG_COLDBOLT);
+
+	// 	expect("it should start %s", fake_sc_start_val.val2, MG_COLDBOLT,
+	// 		skill->get_name(MG_COLDBOLT));
+	// 	expect("it should start %s on level 8", fake_sc_start_val.val3, 8,
+	// 		skill->get_name(MG_COLDBOLT));
+
+	// 	reset_mocks();
+	// 	clear_pc(sd);
+	// }
+
+	return passed;
+}
+
+#endif
 
 void server_online(void)
 {
